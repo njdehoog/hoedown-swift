9926f2a2a3762310ef6cc2bc2490b5d235aa71ed
Niels de Hoog <njdehoog@gmail.com>
2015-05-04 19:34:58 +0200

    Added framework project

---
+++ swift-hoedown/swift-hoedown.h
@@ -0,0 +1,19 @@
+//
+//  swift-hoedown.h
+//  swift-hoedown
+//
+//  Created by Niels de Hoog on 04/05/15.
+//  Copyright (c) 2015 Invisible Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift-hoedown.
+FOUNDATION_EXPORT double swift_hoedownVersionNumber;
+
+//! Project version string for swift-hoedown.
+FOUNDATION_EXPORT const unsigned char swift_hoedownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_hoedown/PublicHeader.h>
+
+
